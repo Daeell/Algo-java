49c43d5a0fd584111db559d4786f2bc5c8c9607e
Kim Dael <daelkdev@gmail.com>
2023-05-01 15:58:44 +0900

    Q2439

---
+++ src/baekjoon/Q2439.java
@@ -0,0 +1,25 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Q2439 {
+    public static void main(String[] args) throws IOException{
+        int n = input();
+        output(n);
+    }
+
+    static int input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        return Integer.parseInt(br.readLine());
+    }
+
+    static void output(int n) {
+        for (int i = 1; i <= n; i++) {
+            String spaces = " ".repeat(n - i);
+            String stars = "*".repeat(i);
+            System.out.println(spaces + stars);
+        }
+    }
+}
