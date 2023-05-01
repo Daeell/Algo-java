a10cfaa4cb827b4932ea8a1cf71b4a42ab0221ba
Kim Dael <daelkdev@gmail.com>
2023-05-01 14:53:52 +0900

    Q2741

--- src/baekjoon/Q2741.java
+++ src/baekjoon/Q2741.java
@@ -1,4 +1,18 @@
 package baekjoon;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Q2741 {
+    public static void main(String[] args) throws IOException {
+        File inputFile = new File("src/baekjoon/input.txt");
+        Scanner sc = new Scanner(inputFile);
+//        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+
+        for (int i = 1; i <= a; i++) {
+            System.out.println(i);
+        }
+    }
 }
